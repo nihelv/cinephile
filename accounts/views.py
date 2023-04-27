3af26bb437a6d339ea4f8c1a1797c5754b4a52d4
YongJuDo <qud6330@naver.com>
2023-04-27 17:06:22 +0900

    marge

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,94 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import AuthenticationForm
+from .forms import CustomUserCreationForm, CustomUserChangeForm
 
-# Create your views here.
+
+# 로그인 view
+def login(request):
+    if request.user.is_authenticated:
+        return redirect('posts:index')
+    
+    if request.method == 'POST':
+        form = AuthenticationForm(request,request.POST)
+        if form.is_valid():
+            auth_login(request, form.get_user())
+            return redirect('posts:index')
+    else:
+        form = AuthenticationForm()
+    
+    context = {
+        'form': form,
+    }
+
+    return render(request, 'accounts/login.html', context)
+
+
+# 로그아웃 view
+def logout(request):
+    if request.user.is_authenticated:
+        auth_logout(request)
+    return redirect('posts:index')
+
+
+# 회원가입 view
+def signup(request):
+    if request.user.is_authenticated:
+        return redirect('posts:index')
+    
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            auth_login(request, user)
+            return redirect('posts:index')
+    else:
+        form = CustomUserCreationForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'acconuts/signup.html', context)
+
+
+# 프로필 view
+@login_required
+def profile(request, username):
+    User = get_user_model()
+    person = User.objects.get(username=username)
+    context = {
+        'person': person,
+    }
+    return render(request, 'accounts/profile.html', context)
+
+
+# 프로필 수정 view
+@login_required
+def update(request):
+    if request.method == 'POST':
+        form = CustomUserChangeForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+        return redirect('posts:index')
+    else:
+        form = CustomUserChangeForm(instance=request.user)
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/update.html', context)
+
+
+# 팔로잉 view
+def follow(request, user_pk):
+    User = get_user_model()
+    person = User.objects.get(pk=user_pk)
+
+    if person != request.user:
+        if request.user in person.followers.all():
+            person.followers.remove(request.user)
+        else:
+            person.followers.add(request.user)
+            
+    return redirect('accounts:profile', person.get_username)
