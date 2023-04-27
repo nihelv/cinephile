67648c55a980bdf72672df34923edf4e2923eabc
Rilee-0320 <go13209@naver.com>
2023-04-27 17:22:23 +0900

    posts update

--- posts/urls.py
+++ posts/urls.py
@@ -1,21 +1,16 @@
-"""cinephile URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
 from django.urls import path
+from . import views
 
+app_name = 'posts'
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
+    path('movie_detail', views.movie_detail, name='movie_detail'),
+    path('create/', views.create, name='create'),
+    path('<int:post_pk>/', views.post_detail, name='post_detail'),
+    path('<int:post_pk>/update', views.update, name='update'),
+    path('<int:post_pk>/delete/', views.delete, name='delete'),
+    path('<int:post_pk>/likes/', views.post_likes, name='post_likes'),
+    path('<int:post_pk>/comments/', views.comment_create, name='comment_create'),
+    path('<int:post_pk>/comments/<int:comment_pk>/delete', views.comment_delete, name='comment_delete'),
+    path('<int:post_pk>/comments/<int:comment_pk>/likes', views.comment_likes, name='comment_likes'),
 ]
