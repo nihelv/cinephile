7f67c9c757d030d526b83bb99ce8ba7f1da7c4e6
Rilee-0320 <go13209@naver.com>
2023-05-04 15:26:43 +0900

    post create template update

--- posts/forms.py
+++ posts/forms.py
@@ -11,30 +11,15 @@ class PostForm(forms.ModelForm):
         label='제목',
         label_suffix='',
         widget=forms.TextInput(
-            attrs={'class': 'form-control form-field',}
+            attrs={'class': 'form-control form-field', 'placeholder': '제목',}
         )
     )
-
-    # score = forms.DecimalField(
-    #     label='평점',
-    #     label_suffix='',
-    #     widget=forms.NumberInput(
-    #         attrs={
-    #             'type': 'number',
-    #             'name': 'score',
-    #             'min': '0',
-    #             'max': '5',
-    #             'step': '0.5',
-    #             'class': 'form-control form-field',
-    #         }
-    #     )
-    # )
 
     content = forms.CharField(
         label='내용',
         label_suffix='',
         widget=forms.Textarea(
-            attrs={'class': 'form-control form-field',}
+            attrs={'class': 'form-control form-field', 'placeholder': '내용',}
         )
     )
 
