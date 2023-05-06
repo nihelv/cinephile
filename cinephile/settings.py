4b9cd90973499396ab913eb95989e2becb680219
Rilee-0320 <go13209@naver.com>
2023-05-06 13:31:20 +0900

    deploy

--- cinephile/settings.py
+++ cinephile/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['port-0-cinephile-1maxx2algn9knde.sel3.cloudtype.app']
 
 
 # Application definition
